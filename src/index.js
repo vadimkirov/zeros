12422ee62992f88da91ddf44ab99f146c1cb088f
vadimkirov <vadimkirov@yandex.ru>
2019-10-02 10:31:44 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function zeros(expression) {
   // your solution
+    let numberFromExpression = expression.split("*"), checkTwo = 0, checkFive = 0;
+    numberFromExpression.forEach(item => {
+        let degreeFactorial;
+        let itselfNumber;
+
+        if (item.slice(-2) === '!!') {
+            degreeFactorial = 2;
+            itselfNumber = +item.slice(0, -2);
+        } else {
+            degreeFactorial = 1;
+            itselfNumber = +item.slice(0, -1);
+        }
+        for(; 0 < itselfNumber ; itselfNumber -= degreeFactorial) {
+
+            let temp = itselfNumber;
+            while (temp % 5 === 0) {
+                checkFive++;
+                temp = temp/5;
+            }
+            temp = itselfNumber;
+            while (temp % 2 === 0) {
+                checkTwo++;
+                temp = temp/2;
+            }
+        }
+    });
+    return Math.min(checkTwo, checkFive);
 }
